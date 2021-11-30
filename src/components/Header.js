50c405e6ac56bc11f6601b034c8f1b6b95fbb6c2
jana6600 <jananikolic6600@gmail.com>
2021-11-30 00:09:56 -0800

    sticky header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,26 @@
 import React, { useCallback, useRef, useState } from 'react'
 import navbar_logo from '../img/navbar_logo.svg'
 import '../styles/header.scss'
-import { BsSearch } from 'react-icons'
-import { AiOutlineUser } from 'react-icons'
-import { BsListNested } from 'react-icons'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BsListNested } from 'react-icons/bs'
 
 const Header = () => {
-  const [isSticky, setIsSticky] = useState('false')
+  // const [isSticky, setIsSticky] = useState('false')
 
-  window.onscroll = function () {
-    scrollFunction()
-  }
+  // window.onscroll = function () {
+  //   scrollFunction()
+  // }
 
-  function scrollFunction() {
-    var showAfter = 10
-    if (this.scrollTop() > showAfter) {
-      setIsSticky(true)
-    }
-  }
+  // function scrollFunction() {
+  //   var showAfter = 10
+  //   if (this.scrollTop() > showAfter) {
+  //     setIsSticky(true)
+  //   }
+  // }
   return (
-    <div className={`navbar_wrapper ${isSticky ? 'sticky_header' : ''} `}>
+    // <div className={`navbar_wrapper ${isSticky ? 'sticky_header' : ''} `}>
+    <div className="navbar_wrapper">
       <div className="navbar_container">
         <a href="/interior" className="Link_wrapper">
           <img src={navbar_logo} className="navbar_logo" />
@@ -27,7 +28,7 @@ const Header = () => {
         <div className="navbar_btns">
           <button className="navbar_button button_1">
             <span className="btn-icon">
-              <BsSearch />
+              <AiOutlineSearch />
             </span>
           </button>
           <button className="navbar_button button_1">
@@ -46,4 +47,4 @@ const Header = () => {
   )
 }
 
-export default Head
+export default Header
